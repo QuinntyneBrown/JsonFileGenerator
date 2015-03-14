b2bcd2f434095e409c73f415345a6fcee9b35485
Quinntyne Brown <qbrown@cetaris.com>
2015-03-14 17:29:26 -0400

    initial commit

---
+++ JsonFileGenerator/Config/AppConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFileGenerator.Config
+{
+    public class AppConfiguration: ConfigurationSection
+    {
+        [ConfigurationProperty("outputPath", IsRequired = true)]
+        public string OutputPath
+        {
+            get { return (string)this["outputPath"]; }
+            set { this["outputPath"] = value; }
+        }
+
+        public static AppConfiguration Config
+        {
+            get { return ConfigurationManager.GetSection("appConfiguration") as AppConfiguration; }
+        }
+    }
+}
---
+++ JsonFileGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using JsonFileGenerator.Models;
+using System.IO;
+using JsonFileGenerator.Config;
+
+namespace JsonFileGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var outputFile = AppConfiguration.Config.OutputPath + "output.json";
+
+            if (File.Exists(outputFile))
+            {
+                File.Delete(outputFile);
+            }
+
+            Product product = new Product
+            {
+                Name = "Apple",
+                Expiry = new DateTime(2008, 12, 28),
+                Price = 3.99M,
+                Sizes = new[] { "Small", "Medium", "Large" }
+            };
+
+            var products = new List<Product>() {product, product};
+            
+            File.WriteAllText(AppConfiguration.Config.OutputPath + "output.json", JsonConvert.SerializeObject(products, Formatting.Indented));
+
+        }
+    }
+}
